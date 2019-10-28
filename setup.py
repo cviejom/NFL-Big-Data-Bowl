0dffce1440242057185dfc4784b0504ac37cecb4
cviejom <carlosviejom@hotmail.com>
2019-10-27 22:00:21 -0600

    chore: Update file structure
    The project file structure has been updated to folow
    the Data Science cookie cutter project template
    
    No issue tracking associated with this commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='How many yards will an NFL player gain after receiving a hand-off ?',
+    author='cviejom',
+    license='MIT',
+)
