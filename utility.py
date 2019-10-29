e0173007e29d9a04ef2adb04f851a79e714372cc
cviejom <carlosviejom@hotmail.com>
2019-10-28 21:01:10 -0600

    chore: Add baseline notebook
    Add a baseline notebook and improvements to the
    utility function to help in the EDA
    
    No issue tracking associated with this commit

--- utility.py
+++ utility.py
@@ -13,7 +13,19 @@ def seed_everything(seed = 42):
 
 
 def read_csv(path, df_name):
-    """ Import a CSV into a pandas df based on the specified path """
+    """ Import a CSV into a Pandas Dataframe based on the specified path """
     df_name = pd.read_csv(path)
     return df_name
     
+
+def describe_categorical(df, sample_fields = 5):
+    ''' Describe categoricals datasets, in more details to have ageneral idea of the values on each column '''
+    text_variables = []
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    df = df.select_dtypes(exclude = numerics)
+    for col in df.columns:
+        text_variables.append(col)
+        pct_valid_data = 1 - (df[col].isnull().sum() / df.shape[0])
+        unique_fields = list(set(df[col]))
+        print('Variable Name: {:<15} % Data: {:0.2f} # Unique Fields:{:<5} Samples: {}'.format(col,pct_valid_data, len(unique_fields), unique_fields[:sample_fields]))
+    return text_variables
